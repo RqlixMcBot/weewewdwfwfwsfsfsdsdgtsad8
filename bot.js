c50a3ec887344598cb26688c3d6d1ae5700f0acb
RqlixMcBot <43500837+RqlixMcBot@users.noreply.github.com>
2018-10-10 00:22:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,11 @@
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
 
 
 
-client.login(process.env.TOKEN);
-
+client.login(process.env.BOT_TOKEN);
 
 
 
@@ -108,7 +108,7 @@ client.on('message', message => {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
     let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
-    let copy = "BroadCast";
+    let copy = "Plus Bot";
     let request = `Requested By ${message.author.username}`;
     if (!args) return message.reply('**يجب عليك كتابة كلمة او جملة لإرسال البرودكاست**');message.channel.send(`**هل أنت متأكد من إرسالك البرودكاست؟ \nمحتوى البرودكاست:** \` ${args}\``).then(msg => {
     msg.react('✅')
